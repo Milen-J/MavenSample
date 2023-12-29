13f7cacb28bc21f287d9fcfacff6ad9d4f382436
lenovo <lenovo@DESKTOP-OIEJOFI>
2023-12-29 12:00:01 +0530

    Ist Program

---
+++ sample1/src/main/java/PackSam1/SampClass1.java
@@ -0,0 +1,15 @@
+package PackSam1;
+
+public class SampClass1 {
+public void add(int a ,int b)
+{
+	System.out.println(a);
+	System.out.println(b);
+	}
+	public static void main(String[] args) {
+		SampClass1 obj1=new SampClass1();
+		obj1.add(10,120);
+
+	}
+
+}
